caf6f3b7597ee9da7eb16e949563b5fc556d214d
K Kiran Kumar <113544336+kiran150502@users.noreply.github.com>
2022-09-22 18:59:47 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,8 @@
+# print('Python'.upper())
+# print('PYThon'.lower())
+# print('HeLLo'.swapcase())
+# print('Cricket'.isupper())
+# print('CRICKET'.isupper())
+# print('cricket'.lower())
+
+print(dir(str))
